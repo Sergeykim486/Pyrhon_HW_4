bc93cd975e95489bbf301cc85ed5c9d80f4a6b80
SergeyKim <sergeykim486@gmail.com>
2022-12-06 17:49:51 +0500

    task 2

---
+++ Task2.py
@@ -0,0 +1,27 @@
+# 2'. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+# 6 -> [1,2,3]
+# 144 -> [2,3]
+
+def Multiplier(n):
+    an = []
+    
+    i = 2
+    while n > 1:
+        while n % i == 0:
+            an.append(i)
+            n //= i
+        i += 1
+
+    result = []
+    for j in an:
+        find = 0
+        for k in result:
+            if k == j:
+                find = 1
+        if find == 0:
+            result.append(j)
+    
+    return an
+
+n = input('Введите число n ->  ')
+print(Multiplier(int(n)))
